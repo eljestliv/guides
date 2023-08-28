afc6b05dc74aaee831b54f8c09f06cd40bc0e409
Liv Lovisa <liv.elina.lovisa@gmail.com>
2023-08-28 18:34:59 +0200

    Create Deinterlace with FFmpeg.bat

---
+++ Deinterlace with FFmpeg.bat
@@ -0,0 +1,10 @@
+Setlocal EnableDelayedExpansion
+set INPUT=E:\assets\eurovision\restoration\1997\MakeMKV\Disc 1
+set OUTPUT=E:\assets\eurovision\restoration\1997\Deinterlaced
+
+:: encode video:
+
+for %%a in ("%INPUT%\*.*") DO ffmpeg -i "%%a" -filter_complex "[0:v]yadif=mode=1[deinterlaced]" -map "[deinterlaced]" -map 0:a -ac 2 -sws_flags lanczos -c:v libx265 -crf 21 -c:a pcm_s24le -map_chapters -1 "%output%\%%~na.mov"
+
+
+
